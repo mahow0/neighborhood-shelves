c82748a146a9f599f83823baf2965f9865e5060c
Tommy <tommy.seng.heng@gmail.com>
2021-09-19 02:38:36 -0400

    Initial Django Server

---
+++ server/model.py
@@ -0,0 +1,11 @@
+
+
+class Model ():
+    """A wrapper method for the trained classifier"""
+    
+    def classify (self, item_name, item_description):
+        """Classifies the item_name and item_description to one of the 21 categories"""
+        return "Grocery"
+
+
+
